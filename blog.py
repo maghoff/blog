5ba0dceb454afeca800c6a3a1f440bc2902354e9
mag <unknown>
2009-10-02 23:18:47 +0200

    Skeleton for a command line tool

---
+++ blog.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main(argv):
+	script = argv[0]
+	argv = argv[1:]
+	cmd_name = "help"
+	if len(argv) > 0:
+		cmd_name = argv[0]
+		argv = argv[1:]
+
+	try:
+		cmd = __import__(cmd_name, globals(), locals(), [], 0)
+	except ImportError:
+		print "Command \"%s\" not found. Try:\n\t%s help\n" % (cmd_name, script)
+		return -1
+
+	return cmd.run(argv)
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
---
+++ help.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import optparse, textwrap
+
+usage = "help [command]"
+desc = "Display documentation about commands"
+
+longhelp = """
+Run help with no arguments to display a list of all available commands.
+
+If the name of a command is given as an argument, detailed help about
+this command will be given.
+"""
+
+def run(argv):
+	p = optparse.OptionParser()
+
+	(options, args) = p.parse_args(argv)
+
+	if len(args) >= 1:
+		tw = textwrap.TextWrapper()
+		tw.initial_indent = '    '
+		tw.subsequent_indent = '    '
+		c = args[0]
+
+		cmd = __import__(c)
+		print "%s\n\n%s\n" % (cmd.usage, cmd.desc)
+		for p in cmd.longhelp.strip().split('\n\n'):
+			p = p.strip()
+			if len(p) == 0: continue
+			print tw.fill(p)
+			print
+	else:
+		# TODO: Find list of available commands
+		cmds = ['help']
+
+		print "Available commands:"
+		for c in cmds:
+			cmd = __import__(c)
+			print "\t%s\t%s" % (c, cmd.desc)
+		print
+
+	return 0
+
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(run(sys.argv[1:]))
