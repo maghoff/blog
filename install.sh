7a58309a47950489745cbc0399d1deedb0f38c71
Unknown <mag@galskap>
2009-10-21 22:51:54 +0200

    You DO need python

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo sudo apt-get install python-gdata markdown
-sudo apt-get install python-gdata markdown
+echo sudo apt-get install python python-gdata markdown
+sudo apt-get install python python-gdata markdown
 
 mkdir -p ~/bin
 ln -s `pwd`/blog.py ~/bin/blog
