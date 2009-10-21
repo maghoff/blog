cf677ba495f2ed9b0084382470a6bd407efedb12
Unknown <mag@galskap>
2009-10-21 23:36:12 +0200

    Added a descriptive comment to install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 echo sudo apt-get install python python-gdata markdown
 sudo apt-get install python python-gdata markdown
 
+
+# The following assumes that ~/bin will be put in your PATH by ~/.profile. Ubuntu
+# does this by default, but only when ~/bin exists, so you might want to re-login.
+
 mkdir -p ~/bin
 ln -s `pwd`/blog.py ~/bin/blog
